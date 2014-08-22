2d415264a35fedba97687feb80f8dedb064adc6a
paulinpaulin007@gmail.com <paulinpaulin007@gmail.com@9dc67481-44da-770e-2b5b-6624a78b5d8a>
2014-08-22 01:59:10 +0000

    add a wifi view

---
+++ WifiLocalization/src/wifi/WifiView.java
@@ -0,0 +1,72 @@
+package wifi;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class WifiView extends View{
+	
+	private float x=400;
+	private float y=400;
+	private int r=20;
+	private Paint mPaint;
+
+	public WifiView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		init();
+	}
+
+	public WifiView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+	
+	public WifiView(Context context) {
+		super(context);
+   init();
+	}
+	
+	private void init(){
+		
+		mPaint = new Paint(); // 3
+		mPaint.setColor(0xffff0000);
+		mPaint.setStrokeWidth(6);
+
+		
+		
+		
+OnTouchListener onTouchListener = new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+//				 Log.d(TAG, "onTouch!" + event);
+//				 Log.d(TAG, "onTouch!" + event.getAction());
+				 int action = event.getAction();
+				 if(action== MotionEvent.ACTION_DOWN|| action == MotionEvent.ACTION_MOVE){
+						x = event.getX();
+						y = event.getY();
+				Log.d("", x+"  "+y);		
+						
+					}
+				return true;
+			}
+		};
+		
+		setOnTouchListener(onTouchListener);
+		
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		
+		super.onDraw(canvas);
+		canvas.drawCircle(x, y, r, mPaint);
+		
+		postInvalidateDelayed(20);
+	}
+
+}
